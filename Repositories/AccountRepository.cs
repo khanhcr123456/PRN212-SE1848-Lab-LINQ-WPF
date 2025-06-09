4421575d798aac87ea6fe2c232353ca3704cd3ef
khanhcr123456 <khanhhdse180719@fpt.edu.vn>
2025-06-09 23:21:09 +0700

    Initial commit

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer;
+
+namespace Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public BusinessObjects.AccountMember GetAccountById(string accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountMember GetAccountMember(string accountID) => AccountDAO.GetAccountById(accountID);
+
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using BusinessObjects;
+using DataAccessLayer;
+
+namespace Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductDAO _productDAO;
+
+        public ProductRepository()
+        {
+            _productDAO = new ProductDAO();
+        }
+
+        public void DeleteProduct(BusinessObjects.Product p) =>
+            _productDAO.DeleteProduct(ConvertToDataAccessProduct(p));
+
+        public void SaveProduct(BusinessObjects.Product p) =>
+            _productDAO.SaveProduct(ConvertToDataAccessProduct(p));
+
+        public void UpdateProduct(BusinessObjects.Product p) =>
+            ProductDAO.UpdateProduct(ConvertToDataAccessProduct(p));
+
+        public List<BusinessObjects.Product> GetProducts() =>
+            ConvertToBusinessProductList(_productDAO.GetProducts());
+
+        public BusinessObjects.Product GetProductById(int id) =>
+            ConvertToBusinessProduct(_productDAO.GetProductById(id));
+
+        // Helper methods to convert between types
+        private DataAccessLayer.Product ConvertToDataAccessProduct(BusinessObjects.Product p)
+        {
+            if (p == null) return null;
+
+            return new DataAccessLayer.Product
+            {
+                ProductID = p.ProductId,
+                ProductName = p.ProductName,
+                CategoryID = p.CategoryId,
+                UnitsInStock = p.UnitsInStock,
+                UnitPrice = p.UnitPrice
+            };
+        }
+
+        private BusinessObjects.Product ConvertToBusinessProduct(DataAccessLayer.Product p)
+        {
+            if (p == null) return null;
+
+            return new BusinessObjects.Product(
+                p.ProductID,
+                p.ProductName,
+                p.CategoryID ?? 0,
+                p.UnitsInStock ?? 0,
+                p.UnitPrice ?? 0);
+        }
+
+        private List<BusinessObjects.Product> ConvertToBusinessProductList(List<DataAccessLayer.Product> products)
+        {
+            List<BusinessObjects.Product> result = new List<BusinessObjects.Product>();
+
+            foreach (var p in products)
+            {
+                result.Add(ConvertToBusinessProduct(p));
+            }
+
+            return result;
+        }
+    }
+}
